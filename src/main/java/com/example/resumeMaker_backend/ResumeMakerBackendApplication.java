45280904b8d16b471a96ab55c43d83b5dea4089f
Sailesh-developer <sailesh.developer2001@gmail.com>
2025-06-01 19:19:59 +0530

    Initial commit

---
+++ src/main/java/com/example/resumeMaker_backend/ResumeMakerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeMaker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeMakerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeMakerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resumeMaker_backend/ResumeMakerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeMaker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeMakerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
